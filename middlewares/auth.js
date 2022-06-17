9d50d2438a838ec5239a33ba6216d7438223cc02
Tatiana <efimenko-ts@mail.ru>
2022-06-17 12:50:18 +0300

    тест авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const generateToken = (payload) => jwt.sign(payload, 'some-secret-key', { expire
 const isAuthorized = (req, res, next) => {
   const auth = req.headers.authorization;
   if (!auth || !auth.startsWith('Bearer ')) {
-    const err = new Error('Authorization error: try again');
+    const err = new Error('Authorization error2: try again');
     err.statusCode = 401;
     throw err;
   }
@@ -14,7 +14,7 @@ const isAuthorized = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (e) {
-    const err = new Error('Authorization error - please, try again');
+    const err = new Error('Authorization error2 - please, try again');
     err.statusCode = 401;
     return next(err);
   }
